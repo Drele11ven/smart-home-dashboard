f2a33a5e31bf3e77734f9e7186f427aa99b8da25
Drele11ven <csgomahdizare@gmail.com>
2025-12-14 18:09:53 +0330

    Comfort & Policy Parameters

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-import streamlit as st
 
-st.set_page_config(page_title="Smart Home Simulator", layout="wide")
-st.title("Smart Home Simulator")
-st.success("Environment setup successful.")
+from core.models import House
+
+house = House()
+room = house.rooms["Living Room"]
+
+room.temp_sensor.set(28)
+room.presence_sensor.set(True)
+
+room.lamp.turn_on()
+room.ac.turn_on(23)
+
+print(room.temperature)
+print(room.lamp.on, room.ac.target_temp)
---
+++ data/preferences.py
@@ -0,0 +1,6 @@
+COMFORT = {
+    "temp_min": 20,
+    "temp_max": 26,
+    "light_min": 40,
+    "eco_temp": 26
+}
